ec9d9ba6c25d53fdb1ea9d9a11f05c38bbbf4208
alexcarchiar <alexcarchiar@gmail.com>
2018-12-13 09:59:00 +0100

    Create ex03.c

---
+++ Lab10/ex03.c
@@ -0,0 +1,59 @@
+//Lab10 ex3
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+#include <math.h>
+
+//definitions
+struct _POINTS {
+    float x;
+    float y;
+    float lenght;
+};
+typedef struct _POINTS POINT;
+#define NUM_POINTS 4
+
+//protos
+
+
+int main(int argc, char **argv){
+    
+    if(argc > 2){
+        printf("Too many arguments inserted\n");
+        exit(1);
+    } else if(argc < 2){
+        printf("Too few arguments inserted\n");
+        exit(1);
+    }
+    
+    POINT coordinates[NUM_POINTS];
+    
+    //Input
+    printf("Insert coordinates");
+    for(int i = 0; i<NUM_POINTS; ++i){
+        printf("\nPoint %d\nx : ", i);
+        scanf("%g", &coordinates[i].x);
+        printf("\ny: ");
+        scanf("%g", &coordinates[i].y);
+        coordinates[i].lenght = sqrtf((coordinates[i].x * coordinates[i].x + coordinates[i].y * coordinates[i].y));
+    }
+    float path = 0;
+    float min = coordinates[0].lenght;
+    if(strcmp(argv[1], "-m")){
+        for(int i = 0; i<NUM_POINTS; ++i){
+            path += coordinates[i].lenght;
+        }
+        printf("The lenght of the path is: %g\n", path);
+    }else if(strcmp(argv[2], "-a")){
+        for(int i = 1; i<NUM_POINTS; ++i){
+            if(min > coordinates[i].lenght)
+                min = coordinates[i].lenght;
+        }
+        printf("Minimum lenght is: %g\n", min);
+    }else {
+        printf("Error: wrong parameter\n");
+    }
+    
+    return 0;
+}
