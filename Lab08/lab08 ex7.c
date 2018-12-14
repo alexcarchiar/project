2d09e3dbc9ec988f2ade813749e7298f997ea474
alexcarchiar <alexcarchiar@gmail.com>
2018-12-14 14:28:27 +0100

    Update lab08 ex7.c

--- Lab08/lab08 ex7.c
+++ Lab08/lab08 ex7.c
@@ -11,7 +11,7 @@ int main(){
     int number;
     int lenght_of_sequence;
     int matrix[SIZE_MAT][SIZE_MAT];
-    
+
     for(int i = 0; i<SIZE_MAT; ++i){ //scanning matrix
         printf("Insert row index %d\n", i);
         for(int j = 0; j<SIZE_MAT; ++j)
@@ -31,52 +31,37 @@ int main(){
         printf("\n");
     }
 
-    
+    // searching rows
+    for(int i=0; i<SIZE_MAT; i++){
+        int j = 0;
+        int found = 0;
+            for(int j = 0; j<SIZE_MAT && !found; ++j){
+                int counter = 0;
 
-    /*for(int i = 0; i<SIZE_MAT; ++i){ //checking rows
-        for(int j = 0; j<SIZE_MAT; ++j){
-            if(matrix[i][j] == number && matrix[i][j+1] == number && matrix[i][j+2] == number){
-                printf("The sequence is found in row %d\n", i);
-                j = SIZE_MAT;
-            }
-        }
-    }
-
-    for(int i = 0; i<SIZE_MAT; ++i){ //checking coloumns
-        for(int j = 0; j<SIZE_MAT; ++j){
-            if(matrix[j][i] == number && matrix[j+1][i] == 0 && matrix[j+2][i]){
-                printf("The sequence is found in coloumn %d\n", i);
-                j = SIZE_MAT;
-            }
-        }
-    } */
-
-    for(int i = 0; i<SIZE_MAT; ++i){ //checking rows
-        int equal = 0;
-        for(int j = 0; j<SIZE_MAT; ++j){
-            if(matrix[i][j] == number){
-                for(int k = j; k<j+lenght_of_sequence; ++k){ //once it finds a value equal to number, it checks if the subsequent values are also equal up to the desired lenght of the sequence
-                    if(matrix[i][k] == number)
-                        ++equal;
+                for(int k = j; matrix[i][k] == number && !found; ++k){ //it picks up a subsequence and tries to find if it is of the required lenght
+                    counter++;
+                    if (counter >= lenght_of_sequence) {
+                    printf("Sequence found in row %d\n", i);
+                    found = 1;
+                }
                 }
             }
-            if(equal == lenght_of_sequence)
-                printf("The sequence is found in row %d\n", i);
-        }
     }
+    //searching in a coloums
+    for(int i=0; i<SIZE_MAT; i++){
+        int j = 0;
+        int found = 0;
+            for(int j = 0; j<SIZE_MAT && !found; ++j){
+                int counter = 0;
 
-    for(int i = 0; i<SIZE_MAT; ++i){ //checking coloumns
-        int equal = 0;
-        for(int j = 0; j<SIZE_MAT; ++j){
-            if(matrix[j][i] == number){
-                for(int k = j; k<j+lenght_of_sequence; ++k){ //once it finds a value equal to number, it checks if the subsequent values are also equal up to the desired lenght of the sequence
-                    if(matrix[k][i] == number)
-                        ++equal;
+                for(int k = j; matrix[k][i] == number && !found; ++k){ //it picks up a subsequence and tries to find if it is of the required lenght
+                    counter++;
+                    if (counter >= lenght_of_sequence) {
+                    printf("Sequence found in coloumn %d\n", i);
+                    found = 1;
+                }
                 }
             }
-            if(equal == lenght_of_sequence)
-                printf("The sequence is found in coloumn %d\n", i);
-        }
     }
     return 0;
 }
